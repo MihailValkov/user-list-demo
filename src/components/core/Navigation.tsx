26e23849a4f895caebac5b78e27d9071c6651659
mihailValkov <mihail_valko@mail.bg>
2022-05-28 12:25:40 +0300

    added footer, header and layout components

---
+++ src/components/core/Navigation.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import styles from './Navigation.module.css';
+
+const Navigation: FC = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.toLocaleString('default', { month: 'long' });
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <span className={styles.text}>React Course - {month} {year} - User List Demo</span>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
