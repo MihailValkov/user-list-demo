c3da5511a0bb12a2b24717567d0728d94e581f90
mihailValkov <mihail_valko@mail.bg>
2022-06-26 23:25:38 +0300

    [CLIENT] minor fixes - css styles;

--- client/src/components/core/Navigation.tsx
+++ client/src/components/core/Navigation.tsx
@@ -6,13 +6,12 @@ const Navigation: FC = () => {
   const date = useMemo(() => new Date(), []);
   const year = date.getFullYear();
   const month = date.toLocaleString('default', { month: 'long' });
-  
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <span className={styles.text}>
-          React Course - {month} {year} - User List Demo
-        </span>
+        <span className={styles.course}>React Course - {month} {year}</span>
+        <span className={styles.description}> User List Demo</span>
       </div>
     </header>
   );
